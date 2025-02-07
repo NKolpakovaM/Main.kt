89cdd782aa699aabd3a5e4bbdcaad4f1588ba8ca
Наташа <KolpakovaNM@yandex.ru>
2025-02-07 17:16:38 +0500

    Initial commit

---
+++ For/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val toSpaceYear : Int
+    toSpaceYear = 1961
+    val toSpaceHour : Int
+    toSpaceHour = 9
+    val toSpaceMinute : Int
+    toSpaceMinute = 7
+    val outSpaceHour : Int
+    outSpaceHour = 10
+    val outSpaceMinute : Int
+    outSpaceMinute = 55
+    println ("Gagarin flight time was:")
+    println ("in $toSpaceYear year")
+    println ("at $toSpaceHour hours")
+    println ("and $toSpaceMinute minutes")
+    print ("Gagarin landing time was: $outSpaceHour:$outSpaceMinute")
+
+}
