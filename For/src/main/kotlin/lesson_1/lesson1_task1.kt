686e7c850b9f423f646648c9838ed927a71717da
Наташа <KolpakovaNM@yandex.ru>
2025-02-09 00:19:20 +0500

    KS-1-1 create smth

--- For/src/main/kotlin/lesson_1/lesson1_task1.kt
+++ For/src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,4 +1,4 @@
 fun main() {
     val orderNumber = 75
-    println ("Thank you for using our services! You have already booked $orderNumber orders!")
+    println ("Thanks for using our services! You have already booked $orderNumber orders!")
 }
